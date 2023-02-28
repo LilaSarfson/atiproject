3a45d9ea620f84389378d53986829035c095bb17
LilaSarfson <lilamandarina@hotmail.com>
2023-02-28 21:33:08 +0100

    fecth done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import 'devextreme/dist/css/dx.light.css';
-import dataPay from '../src/data/Payload.json'
+import { useEffect, useState } from 'react';
+import dataPay from '../src/data/payload.json'
+import { fetchData } from './api/service';
 import Card from './components/Card';
 function App() {
-  const Payload = dataPay.Payload[3];
-  const machines = Payload.workunits
-  console.log(Payload)
+  useEffect(()=>{
+    setData(fetchData())
+  }, [])
+  const [data, setData] = useState('')
+
+  const machinesData = dataPay.Payload[3].workunits
+
   return (
     <div className="App">
       <div className='flex flex-row flex-wrap gap-4'>
       {
-        machines.map((mach) =>{return(<Card
+        machinesData.map((mach, index) =>{return(<Card
+        key={index}
         value1="tprod"
         value2="tpar"
         value3="tprep"
         title={mach.wkuname}
-        data={machines}
+        data={machinesData}
         speed={mach.speed}
         button = {mach.sitname}
         order={mach.wohnumber}
---
+++ src/api/service.js
@@ -0,0 +1,26 @@
+export function fetchData (){
+    const URL= "http://intranet.taionline.net:14036/api/loadtfunction"
+    fetch(URL, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+    body: {
+            "FuncName": "Tai.Backend.Qplant",
+            "ActiveActor": "WKU11",
+            "FuncParam01": "OEEMONITOR2",
+            "FuncParam02": "SPAIN",
+            "FuncParam03": "",
+            "FuncParam04": "",
+            "FuncParam05": "",
+            "TraceLog": "Y"
+        
+            }
+    }).then(response => {
+        console.log(response)
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+}
