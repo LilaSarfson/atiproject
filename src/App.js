d6ce79d70296877196f4a684ac915bd09e2ab7be
Lila Sarfson <lilamandarina@hotmail..com>
2023-03-03 09:27:59 +0100

    un detallito de css

--- src/App.js
+++ src/App.js
@@ -21,27 +21,29 @@ function App() {
     return resultado
   }
   return (
-    <div id="App" className="App flex flex-col justify-center gap-8 mt-4">
-      <h1 className='text-center'>ATI<span className='text-red-600 underline underline-offset-1'>smartfactory</span></h1>
-     <div className='flex flex-wrap gap-6 justify-center w-full'>
-      {
-        //Uso la función map para pintar el componente Card tantas veces como elementos haya en el array de dataMachines.
-        dataMachines.map((mach, index) =>{return(<Card
-        key={index}
-        value1="tprod"
-        value2="tpar"
-        value3="tprep"
-        title={mach.wkuname}
-        data={FilterChartData(index)}
-        speed={mach.speed}
-        button = {mach.sitname}
-        order={mach.wohnumber}
-        product={mach.matcode}
-        pname={mach.matname}
-        required={mach.quantityrequired}
-        produced={mach.quantityproduced}
-        />)}) 
-      }
+    <div id="App" className='App'>
+      <div  className="flex flex-col gap-8 my-4">
+        <h1 className='text-center'>ATI<span className='text-red-600 underline underline-offset-1'>smartfactory</span></h1>
+        <div className='flex flex-wrap gap-6 justify-center w-full'>
+        {
+          //Uso la función map para pintar el componente Card tantas veces como elementos haya en el array de dataMachines.
+          dataMachines.map((mach, index) =>{return(<Card
+          key={index}
+          value1="tprod"
+          value2="tpar"
+          value3="tprep"
+          title={mach.wkuname}
+          data={FilterChartData(index)}
+          speed={mach.speed}
+          button = {mach.sitname}
+          order={mach.wohnumber}
+          product={mach.matcode}
+          pname={mach.matname}
+          required={mach.quantityrequired}
+          produced={mach.quantityproduced}
+          />)}) 
+        }
+        </div>
       </div>
     </div>
   );
